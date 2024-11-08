49ef6759758fa2a5176a038c8c87279a32a0864a
maricarmen5 <mcarmen1804@gmail.com>
2024-11-08 09:56:29 +0100

    Commit de Prueba 2

--- src/edu/epsevg/prop/lab/c4/Juga2.java
+++ src/edu/epsevg/prop/lab/c4/Juga2.java
@@ -96,7 +96,7 @@ public class Juga2 extends javax.swing.JFrame {
 
         // Definiu al vostre gust els jugadors a enfrontar.
         Jugador p1 = new Aleatori();
-        //Jugador p1 = new Aleatori(); //
+        //Jugador p1 = new Aleatori(); 
 
         Jugador p2 = new Profe(2,false);
         //Jugador p2 = new Manual();
